173dcb87cf05571c5c525e9ad18592197cee10c5
Ryan Gerber <ryangerber1990@gmail.com>
2019-01-05 20:12:58 +0200

    Push from linux CLI

---
+++ app/src/main/java/com/mass/creative/mysqlexmaple/user_signup.java
@@ -0,0 +1,13 @@
+package com.mass.creative.mysqlexmaple;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class user_signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_signup);
+    }
+}
