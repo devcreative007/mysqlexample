69f226171be577161871f7538d2d8dad469f6b98
Ryan Gerber <ryangerber1990@gmail.com>
2019-01-06 15:12:00 +0200

    Added home screen

--- app/src/main/java/com/mass/creative/mysqlexmaple/MainActivity.java
+++ app/src/main/java/com/mass/creative/mysqlexmaple/MainActivity.java
@@ -1,7 +1,10 @@
 package com.mass.creative.mysqlexmaple;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,8 +12,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        Button signupButton = findViewById(R.id.signUpBtn);
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,user_signup.class));
+            }
+        });
+
+        Button loginButton = findViewById(R.id.loginBtn);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(login()){
+
+                }else
+                {
+                    //TODO: Make TOAST
+                }
+            }
+        });
+
+    }
+    public Boolean login(){
+
+        return false;
+    }
 
 
 
